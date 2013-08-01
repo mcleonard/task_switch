41ffdc79bec922e9465e9323e4ba5c618c41b164
Mat Leonard <leonard.mat@gmail.com>
2013-07-31 17:51:33 -0700

    Mostly adding comments.  Changed plot_state for the better.

--- hopfield.py
+++ hopfield.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class Hopfield(object):
 
 	def __init__(self, state=None, W=None):
-		self.W = W					# Weight matrix
+		self.W = W	# Weight matrix
 		self.state = state
 		self.trained_states = 0
 		self.shape = None
@@ -15,8 +15,10 @@ class Hopfield(object):
 		""" Run the Hopfield network using simulated annealing. """
 		
 		for t in range(n_iters):
-			cur_temp = init_temp*np.exp(-t/10)
-			self.update(tau=cur_temp)
+			curr_temp = 0.15 + init_temp*np.exp(-t/5.)
+			self.update(tau=curr_temp)
+
+		return self.state
 
 	def update(self, tau=0.1):
 		""" Update all the units once. """
@@ -27,17 +29,20 @@ class Hopfield(object):
 		# Then take the threshold and change to 1, -1 from 1, 0
 		new_state = (np.random.rand(len(self)) < ps)*2 - 1
 		self.state = new_state
+		return self.state
 
 	def train(self, Y):
 		""" Train the weight matrix W on input Y. """
 		
 		n = self.trained_states
+
+		# This performs the actual Hebbian learning
 		new_W = np.array([ y.repeat(len(Y))*Y for y in Y])
 		
 		if self.W is None:
 			self.W = new_W
 		else:
-			old_W = self.W*self.trained_states
+			old_W = self.W*n
 			self.W = 1/float(n+1) * (old_W + new_W)
 		
 		self.trained_states += 1
@@ -45,12 +50,13 @@ class Hopfield(object):
 		# Make sure diagonal is 0
 		self.W = self.W - np.diag(self.W.diagonal())
 
-	def plot(self, shape=None, fig_n=None):
-		if shape is None and self.shape is not None:
+	def plot(self, shape=None, fig_num=None):
+		if shape is not None:
+			pass
+		elif self.shape is not None:
 			shape = self.shape
-		else:
-			shape = self.state.shape
-		plot_state(self.state, shape=shape, fig_n=fig_n)
+		
+		plot_state(self.state, shape=shape, fig_num=fig_num)
 
 	def __len__(self):
 		return len(self.state)
@@ -67,7 +73,7 @@ class Hopfield(object):
 			return False
 
 	def __repr__(self):
-		return str(self.state)
+		return self.state.__repr__()
 
 def random_state(N=100):
 	""" Generate a random state with N units. """
@@ -84,19 +90,26 @@ def sigmoid(x):
 	y = 1/(1 + np.exp(-x))
 	return y
 
-def plot_state(state, shape=None, fig_n=None):
+def plot_state(state, shape=None, fig_num=None):
 	""" Plots the given state as an image of squares indicating the state of 
 		each unit. 
 	"""
 
-	if fig_n:
-		fig = plt.figure(fig_n)
+	if fig_num:
+		fig = plt.figure(fig_num)
 		fig.clf()
 	else:
 		fig = plt.figure()
 	ax = fig.add_subplot(111)
-	
+
+	if shape is None:
+		shape = (1, len(state))
+
 	s = state.reshape(shape)
-	ax.imshow(s, interpolation='nearest', cmap=plt.cm.gray)
-	ax.set_xticklabels('')
-	ax.set_yticklabels('')
+	_ = ax.imshow(s, interpolation='nearest', cmap=plt.cm.gray)
+	_ = ax.set_xticks(np.arange(-0.5, -0.5+shape[1]))
+	_ = ax.set_yticks(np.arange(-0.5, -0.5+shape[0]))
+	_ = ax.set_xticklabels('')
+	_ = ax.set_yticklabels('')
+	_ = ax.grid(b=True, which='major', color='white', ls='-')
+	ax.tick_params(length=0)
