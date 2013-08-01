d5e9f556bf1ec7d095efb785ad219d2c6fde570c
Mat Leonard <leonard.mat@gmail.com>
2013-07-31 17:49:41 -0700

    Removed HIT and ERROR

--- constants.py
+++ constants.py
@@ -3,23 +3,20 @@
 import numpy as np
 import hopfield
 
-HIT = 1
-ERROR = 0
-
 # Defining states for the Hopfield network
-colors = {'red':np.array([1,1,1,1, -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]),
+colors = {'red':np.array([1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]),
 		  'blue':np.array([-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1]),
 		  'green':np.array([-1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1])}
 
 words = {'red':np.array([1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1]),
-		 'blue':np.array([-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1, 1,-1,-1]),
+		 'blue':np.array([-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1]),
 		 'green':np.array([-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1])}
 
 # Training Weight matrix for color outputs
 net = hopfield.Hopfield()
 for c in colors:
 	net.train(colors[c])
-W_ink = net.W
+W_clr = net.W
 
 # Training Weight matrix for word outputs
 net = hopfield.Hopfield()
